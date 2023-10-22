bc73a444f7d19e14fd59d01eede74847ec1630e5
Rad Warmbold <cradbold@gmail.com>
2023-10-22 10:48:53 -0700

    shell --> comp/shell dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Shell from './Shell';
+import Shell from './components/shell/Shell';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
