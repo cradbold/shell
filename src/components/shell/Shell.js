bf6136cac0a3911e2f155e929338c6af9df664d8
Rad Warmbold <cradbold@gmail.com>
2023-10-22 12:18:08 -0700

    Revert "add empty header"
    This reverts commit 4f2dd159cc2713ae8fa35a4cdadfd05319a1374f.

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -2,14 +2,12 @@
 import './Shell.css';
 import Header from '../header/Header'
 import Body from '../body/Body'
-import Footer from '../footer/Footer'
 
 function Shell() {
   return (
     <div className="Shell">
       <Header />
       <Body />
-      <Footer />
     </div>
   );
 }
