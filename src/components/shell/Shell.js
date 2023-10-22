43eab0be3a93681dd385c612cd7f419656897b0b
Rad Warmbold <cradbold@gmail.com>
2023-10-22 12:14:53 -0700

    add empty header

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './Shell.css';
+import Header from '../header/Header'
 import Body from '../body/Body'
 
 function Shell() {
   return (
     <div className="Shell">
-      <header />
+      <Header />
       <Body />
     </div>
   );
