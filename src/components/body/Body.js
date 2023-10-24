62a937dcf1eafc1ad7c516d1218a4ccab41f164a
Rad Warmbold <cradbold@gmail.com>
2023-10-24 16:10:28 -0700

    one prop -> multi-prop state struct

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,19 +3,24 @@ import React from "react";
 import './Body.css';
 
 const Body = () => {
-  const [source, setSource] = React.useState('src/components/body/Body.js');
+  const [state, setState] = React.useState({
+    'source': 'src/components/body/Body.js'
+  });
 
   return (
     <div className="Body">
       <img src={logo} className="Body-logo" alt="logo" />
       <p>
-        Edit <code>{source}</code> and save to reload.
+        Edit <code>{state.source}</code> and save to reload.
       </p>
       <a className="Body-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
       <p>
-        <input value={source} onChange={(e) => setSource(e.target.value)} />
+        <input value={state.source} onChange={(e) => setState({
+          ...state,
+          'source': e.target.value
+        })} />
       </p>
     </div>
   );
