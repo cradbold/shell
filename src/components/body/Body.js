3c805e5ca5d29dbc0df79533877ed6b66275c190
Rad Warmbold <cradbold@gmail.com>
2023-10-24 16:00:54 -0700

    added state

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
+import React from "react";
 import './Body.css';
 
 const Body = () => {
+  const [source, setSource] = React.useState('src/components/body/Body.js');
+
   return (
     <div className="Body">
       <img src={logo} className="Body-logo" alt="logo" />
       <p>
-        Edit <code>src/components/body/Body.js</code> and save to reload.
+        Edit <code>{source}</code> and save to reload.
       </p>
       <a className="Body-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
+      <p>
+        <input value={source} onChange={(e) => setSource(e.target.value)} />
+      </p>
     </div>
   );
 }
