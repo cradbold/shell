8677ba2c876e9e620a11394432add438f8d97708
Rad Warmbold <cradbold@gmail.com>
2023-10-24 18:18:32 -0700

    fix errs; boilerplate todo context done

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import React from "react";
 import './Body.css';
+import { addTodo } from '../../actions';
+import { Store } from '../../store/store';
 
 const Body = () => {
   const [state, setState] = React.useState({
     'source': 'src/components/body/Body.js'
   });
 
-  const changeSource = (e) => {
+  const { dispatch } = React.useContext(Store);
+
+  const changeSource = (event) => {
     setState({
       ...state,
-      'source': e.target.value
+      'source': event.target.value,
+      'todo': 0
     });
-  }
+  };
+
+  const clickTodo = (event) => {
+    event.preventDefault();
+    addTodo(state.todo, dispatch);
+    setState({
+      ...state,
+      'todo': ''
+    });
+    console.log(state)
+  };
 
   return (
     <div className="Body">
@@ -25,6 +40,11 @@ const Body = () => {
       </a>
       <p>
         <input value={state.source} onChange={changeSource} />
+      </p>
+      <p>
+        <button onClick={clickTodo}>
+          To Do
+        </button>
       </p>
     </div>
   );
