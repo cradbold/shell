5809dff8d2faee9475e23a41b245ab8b3dc663ec
Rad Warmbold <cradbold@gmail.com>
2023-10-24 18:01:33 -0700

    added actions placeholder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { StoreProvider } from './store/store';
 import Shell from './components/shell/Shell';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,9 @@ import * as serviceWorker from './serviceWorker';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Shell />
+    <StoreProvider>
+      <Shell />
+    </StoreProvider>
   </React.StrictMode>
 );
 
