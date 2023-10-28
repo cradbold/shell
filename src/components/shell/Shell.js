1fb1ba5c902fb6c85596680c00fd84a1f226b3a1
Rad Warmbold <cradbold@gmail.com>
2023-10-27 18:46:09 -0700

    added global padding

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,14 +1,15 @@
+import './Shell.scss';
 import Header from '../header/Header';
 import Main from '../main/Main';
 import Footer from '../footer/Footer';
 
 const Shell = () => {
   return (
-    <>
+    <div className="Shell">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
