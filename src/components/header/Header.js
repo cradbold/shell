d691f694051905a26ed8e1ea96686f942ba3b12d
Rad Warmbold <cradbold@gmail.com>
2023-10-27 17:32:05 -0700

    Add navbar, nav, navlink placeholders

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,22 @@
 import './Header.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Navbar';
+import NavLink from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
     <div className="Header">
-      <header className="Header-header" />
+      <header className="Header-header">
+        <Navbar>
+          <Navbar.Brand>Absidy Holdings</Navbar.Brand>
+          <Nav>
+            <NavLink href="/dashboard">Dashboard</NavLink>
+          </Nav>
+          <Nav>
+            <NavLink href="/profile">Profile</NavLink>
+          </Nav>
+        </Navbar>
+      </header>
     </div>
   );
 }
