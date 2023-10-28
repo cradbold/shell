901316dea3cd126a43456528d1fb36c5154130c0
Rad Warmbold <cradbold@gmail.com>
2023-10-27 18:10:12 -0700

    remove base index css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { StoreProvider } from './store/store';
 import Shell from './components/shell/Shell';
 import reportWebVitals from './reportWebVitals';
