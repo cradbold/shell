4c76a32283efde250542faa42fc20cc9b1eb4f5d
Rad Warmbold <cradbold@gmail.com>
2023-10-28 13:20:59 -0700

    basic routes

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,14 +1,44 @@
 import './Shell.scss';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from '../header/Header';
 import Main from '../main/Main';
 import Footer from '../footer/Footer';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    )
+  }, {
+    path: "/dashboard",
+    element: (
+      <>
+        <Header />
+        <div>Dashboard!</div>
+        <Footer />
+      </>
+    )
+  }, {
+    path: "/profile",
+    element: (
+      <>
+        <Header />
+        <div>Profile!</div>
+        <Footer />
+      </>
+    )
+  }
+]);
+
 const Shell = () => {
   return (
     <div className="Shell">
-      <Header />
-      <Main />
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   );
 }
