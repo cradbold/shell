e6f2d719a676c4acb712a26dfe4ea5470edc3c2d
Rad Warmbold <cradbold@gmail.com>
2023-10-27 18:35:18 -0700

    remove empty scss

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,15 @@
-import './Header.scss';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Navbar';
-import NavLink from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Header = () => {
   return (
-    <div className="Header">
-      <header className="Header-header">
-        <Navbar>
-          <Navbar.Brand>Absidy Holdings</Navbar.Brand>
-          <Nav>
-            <NavLink href="/dashboard">Dashboard</NavLink>
-          </Nav>
-          <Nav>
-            <NavLink href="/profile">Profile</NavLink>
-          </Nav>
-        </Navbar>
-      </header>
-    </div>
+    <Navbar>
+      <Navbar.Brand>Absidy Holdings</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+        <Nav.Link href="/profile">Profile</Nav.Link>
+      </Nav>
+    </Navbar>
   );
 }
 
