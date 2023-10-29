9922f9beda111870c55d3cd760c96bbd4aedddfb
Rad Warmbold <cradbold@gmail.com>
2023-10-29 15:46:08 -0700

    added react-auth-kit lib

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,6 +1,5 @@
 import './Shell.scss';
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useAuth } from 'react-auth';
 // import Header from '../header/Header';
 // import Main from '../main/Main';
 // import Footer from '../footer/Footer';
@@ -37,25 +36,6 @@ import { useAuth } from 'react-auth';
 // ]);
 
 const Shell = () => {
-  const { isAuthenticated, user, login, logout } = useAuth();
-
-  if (isAuthenticated) {
-    return (
-      <div>
-        <h1>Welcome, {user.name}</h1>
-        <p>You are currently logged in.</p>
-        <button onClick={logout}>Logout</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Please login</h1>
-        <button onClick={login}>Login</button>
-      </div>
-    );
-  }
-
   // return (
   //   <div className="Shell">
   //     <RouterProvider router={router} />
