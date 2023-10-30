63e737b9d5749fd25cbadaa25f55018cabd9e202
Rad Warmbold <cradbold@gmail.com>
2023-10-29 18:08:08 -0700

    working router; revert from react-auth-kit

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,46 +1,55 @@
 import './Shell.scss';
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Header from '../header/Header';
-// import Main from '../main/Main';
-// import Footer from '../footer/Footer';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <>
-//         <Header />
-//         <Main />
-//         <Footer />
-//       </>
-//     )
-//   }, {
-//     path: "/dashboard",
-//     element: (
-//       <>
-//         <Header />
-//         <div>Dashboard!</div>
-//         <Footer />
-//       </>
-//     )
-//   }, {
-//     path: "/profile",
-//     element: (
-//       <>
-//         <Header />
-//         <div>Profile!</div>
-//         <Footer />
-//       </>
-//     )
-//   }
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    )
+  }, {
+    path: "/dashboard",
+    element: (
+      <>
+        <Header />
+        <div>Dashboard!</div>
+        <Footer />
+      </>
+    )
+  }, {
+    path: "/profile",
+    element: (
+      <>
+        <Header />
+        <div>Profile!</div>
+        <Footer />
+      </>
+    )
+  }, {
+    path: "/login",
+    element: (
+      <>
+        <Header />
+        <div>Login</div>
+        <Footer />
+      </>
+    )
+  }
+]);
 
 const Shell = () => {
-  // return (
-  //   <div className="Shell">
-  //     <RouterProvider router={router} />
-  //   </div>
-  // );
+  return (
+    <div className="Shell">
+        <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default Shell;
