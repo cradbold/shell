e7c096937a8943f5b11a4e6491c6900ae3778add
Rad Warmbold <cradbold@gmail.com>
2023-10-30 18:45:41 -0700

    react containers verbatim per react-auth example

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StoreProvider } from './store/store';
-import Shell from './components/shell/Shell';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./App";
 
-// The following line can be included in your src/index.js or App.js file */
-import 'bootstrap/dist/css/bootstrap.min.css';
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+const theme = createTheme({
+  palette: {
+    primary: { main: "#3a34d2" }
+  }
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <StoreProvider>
-      <Shell />
-    </StoreProvider>
-  </React.StrictMode>
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
