da0a1289df02c75b6b42d2d43e72833422099b52
Rad Warmbold <cradbold@gmail.com>
2023-10-31 12:53:30 -0700

    bracket nit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Route, createBrowserRouter, createRoutesFromElements, defer } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
+import { AuthLayout } from "./t_components/AuthLayout";
+import { HomeLayout } from "./t_components/HomeLayout";
+import { ProtectedLayout } from "./t_components/ProtectedLayout";
 import { HomePage } from "./pages/Home";
+import { LoginPage } from "./pages/Login";
 import { ProfilePage } from "./pages/Profile";
 import { SettingsPage } from "./pages/Settings";
-import { ProtectedLayout } from "./t_components/ProtectedLayout";
-import { HomeLayout } from "./t_components/HomeLayout";
-import { AuthLayout } from "./t_components/AuthLayout";
 
 // ideally this would be an API call to server to get logged in user data
 const getUserData = () => {
@@ -18,12 +18,13 @@ const getUserData = () => {
 };
 
 // for error
-// const getUserData = () =>
+// const getUserData = () => {
 //   new Promise((resolve, reject) =>
 //     setTimeout(() => {
 //       reject("Error");
 //     }, 3000)
 //   );
+// };
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
