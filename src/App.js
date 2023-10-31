43551b497883125ea925dbeff20bbe2f4c6c5fea
Rad Warmbold <cradbold@gmail.com>
2023-10-31 10:24:33 -0700

    ws nit

--- src/App.js
+++ src/App.js
@@ -26,15 +26,11 @@ const getUserData = () =>
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      element={<AuthLayout />}
-      loader={() => defer({ userPromise: getUserData() })}
-    >
+    <Route element={<AuthLayout />} loader={() => defer({ userPromise: getUserData() })}>
       <Route element={<HomeLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Route>
-
       <Route path="/dashboard" element={<ProtectedLayout />}>
         <Route path="profile" element={<ProfilePage />} />
         <Route path="settings" element={<SettingsPage />} />
