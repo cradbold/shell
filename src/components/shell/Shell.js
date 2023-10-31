fa9f8753a1f07c0194443c63f84d1d155f2067c9
Rad Warmbold <cradbold@gmail.com>
2023-10-30 18:46:07 -0700

    comps verbatim per react-auth example

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,13 +1,67 @@
+import { useRoutes } from 'react-router-dom';
+// import { RouterProvider } from 'react-router-dom';
 import './Shell.scss';
-import { Router } from '../../routes';
-import { RouterProvider } from 'react-router-dom';
+import { ProtectedRoute } from "../../routes/ProtectedRoute";
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
+// import { Router } from '../../routes';
 
 const Shell = () => {
-  return (
-    <div className="Shell">
-        <RouterProvider router={Router} />
-    </div>
-  );
+  const routes = useRoutes([{
+      path: "/",
+      element: (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+      )
+    }, {
+      path: "/dashboard",
+      element: (
+      <>
+        <ProtectedRoute>
+          <Header />
+          <div>Dashboard!</div>
+          <Footer />
+        </ProtectedRoute>
+      </>
+      )
+    }, {
+      path: "/profile",
+      element: (
+      <>
+        <ProtectedRoute>
+          <Header />
+          <div>Profile!</div>
+          <Footer />
+        </ProtectedRoute>
+      </>
+      )
+    }, {
+      path: "/register",
+      element: (
+      <>
+        <div>Register!</div>
+      </>
+      )
+    }, {
+      path: "/login",
+      element: (
+      <>
+        <div>Login!</div>
+      </>
+      )
+    }
+  ]);
+
+  return routes;
+  // return (
+  //   <div className="Shell">
+  //       <RouterProvider router={Router} />
+  //   </div>
+  // );
 };
 
 export default Shell;
