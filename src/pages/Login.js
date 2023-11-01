ed0a7be62ed77de7edec4c72f2b938f1edc7a532
Rad Warmbold <cradbold@gmail.com>
2023-11-01 12:07:44 -0700

    nit nested comp

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,7 +26,9 @@ export const LoginPage = () => {
   return (
     <Container component="main" maxWidth="xs">
       <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>{<LockOutlinedIcon />}</Avatar>
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
         <Typography component="h1" variant="h5">Log In</Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
