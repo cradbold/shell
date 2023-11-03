88332d7fa0cf3c58c0f0283a7181aa5f3d9bf6f7
Rad Warmbold <cradbold@gmail.com>
2023-11-02 19:38:03 -0700

    remove bootstrap dep

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
 import Button from '@mui/material/Button';
 
 import logo from './logo.svg';
