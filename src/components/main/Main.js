de719cef080f415008ef514b72cc4ade3ee1f0fc
Rad Warmbold <cradbold@gmail.com>
2023-11-04 10:25:37 -0700

    bootstrap -> mui comp/dep in main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 
 import logo from './logo.svg';
@@ -38,15 +39,9 @@ const Main = () => {
     <div className="main">
       <img src={logo} className="main-logo" alt="logo" />
       <p>
-        <Form.Label>Edit <code>{localState.source}</code> and save to reload.</Form.Label>
-        <Form.Control
-          type="text"
-          id="inputSource"
-          aria-describedby="source"
-          onChange={changeSource}
-          value={localState.source}
-        />
-        </p>
+        <InputLabel>Edit <code>{localState.source}</code> and save to reload.</InputLabel>
+        <TextField margin="normal" required fullWidth id="textFieldSource" label="Source" value={localState.source} onChange={changeSource} autoFocus />        
+      </p>
       <p>
         <Button onClick={clickIncrement} className='main-Button-green'>Increment</Button>
         <Button onClick={clickDecrement} className='main-Button-red'>Decrement</Button>
