e36a5e16e8feeda4f80bc0ef9855dd8b4b36b324
Rad Warmbold <cradbold@gmail.com>
2023-11-08 19:49:44 -0800

    moving spin to inline; will make dynamic w/buttons

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -12,6 +12,7 @@ const Main = () => {
   const [localState, setState] = React.useState({
     'source': 'src/components/body/Body.js'
   });
+  const [spin, setSpin] = React.useState("5s");
 
   React.useEffect(() => {
     console.log(`State: ${JSON.stringify(state)}`)
@@ -37,11 +38,13 @@ const Main = () => {
 
   return (
     <div className="main">
-      <img src={logo} className="main-logo" alt="logo" />
-        <InputLabel>Edit <code>{localState.source}</code> and save to reload.</InputLabel>
-        <TextField margin="normal" required fullWidth id="textFieldSource" label="Source" value={localState.source} onChange={changeSource} autoFocus />        
-        <Button onClick={clickIncrement} className='main-Button-green'>Increment</Button>
-        <Button onClick={clickDecrement} className='main-Button-red'>Decrement</Button>
+      <img src={logo} className="main-logo" alt="logo" style={{ animation: `main-logo-spin infinite ${spin} linear` }}/>
+      {/* <Button onClick={clickIncrement} className='main-Button-faster'>Faster</Button>
+      <Button onClick={clickDecrement} className='main-Button-slower'>Slower</Button> */}
+      <InputLabel>Edit <code>{localState.source}</code> and save to reload.</InputLabel>
+      <TextField margin="normal" required fullWidth id="textFieldSource" label="Source" value={localState.source} onChange={changeSource} autoFocus />        
+      <Button onClick={clickIncrement} className='main-Button-green'>Increment</Button>
+      <Button onClick={clickDecrement} className='main-Button-red'>Decrement</Button>
     </div>
   );
 };
